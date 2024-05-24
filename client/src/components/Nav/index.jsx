1ec04ace2f094e9e697c9542292d5b2a64c067a2
Jasmine J <146461479+J-0987@users.noreply.github.com>
2024-05-24 09:47:50 -0400

    auth setup for main page

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,10 +1,11 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import Auth from "../../utils/auth";
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from '../assets/logo.png';
 import './index.css';
-
+import { Link } from 'react-router-dom';
 
 
 function Navigation() {
@@ -15,41 +16,71 @@ function Navigation() {
     'Sunglasses', 'Tablets', 'Tops', 'Vehicle', 'Womens Bags', 'Womens Dresses',
     'Womens Jewellery', 'Womens Shoes', 'Womens Watches'
   ];
+
+  function showNavigation() {
+    if (Auth.loggedIn()) {
+      return (
+        <div className='nav-links'>
+<Nav.Link as={Link} to="/" onClick={() => Auth.logout()}>Logout</Nav.Link>
+
+<Nav.Link as={Link} to="/orderHistory">My Account</Nav.Link> 
+        </div>
+        
+      )
+
+
+
+    } else {
+
+      return (
+        <NavDropdown title="Create Account /Login" id="basic-nav-dropdown">
+          <NavDropdown.Item as={Link} to="/signup">Create Account</NavDropdown.Item>
+          <NavDropdown.Item as={Link} to="/login">
+            Login
+          </NavDropdown.Item>
+
+        </NavDropdown>
+      )
+    }
+
+  }
+
   return (
     <Navbar expand="lg"  className="bg-body-tertiary nav-container">
-      <Container >
-   
-    
-        <Navbar.Brand href="#home">
-         THE SHOP</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            {/* <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link> */}
-            <NavDropdown className="full-width-dropdown" title="Categories" id="basic-nav-dropdown">
-            {categories.map((category, index) => (
-                <NavDropdown.Item key={index} href={`#${category}`}>
-                  {category}
-                  </NavDropdown.Item>
-              ))}
-             
-            </NavDropdown>
+    <Container >
+ 
+  
+      <Navbar.Brand as={Link} to="/">
+       THE SHOP</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          {/* <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Link</Nav.Link> */}
+          <NavDropdown className="full-width-dropdown" title="Categories" id="basic-nav-dropdown">
+          {categories.map((category, index) => (
+              <NavDropdown.Item key={index} href={`#${category}`}>
+                {category}
+                </NavDropdown.Item>
+            ))}
+           
+          </NavDropdown>
 
-            <NavDropdown title="Create Account /Login" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Create Account</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-               Login
-              </NavDropdown.Item>
-              
-            </NavDropdown>
-        
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+        </Nav>
+<Nav>
+{showNavigation()}
+</Nav>
+
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  )
+
+
+
 }
+
+
 
 export default Navigation;
 
