33d3b6f95710a95923f6ef3836babd9037f92e34
Bryan Peens <peensbryan1@gmail.com>
2024-05-27 01:42:25 -0400

    include dotenv, update server, update resolvers, update cart

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -94,24 +94,19 @@ const resolvers = {
     checkout: async (parent, args, context) => {
       try {
         const url = new URL(context.headers.referer).origin;
-        await Order.create({ products: args.products.map(({ _id }) => _id) });
-        const line_items = [];
-
-        for (const product of args.products) {
-          line_items.push({
-            price_data: {
-              currency: 'usd',
-              product_data: {
-                name: product.name,
-                description: product.description,
-                images: [`${url}/images/${product.thumbnail}`],
-              },
-              unit_amount: Math.round(product.price * 100),
+        const line_items = args.products.map(product => ({
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: product.name,
+              description: product.description,
+              images: [`${url}/images/${product.thumbnail}`],
             },
-            quantity: product.purchaseQuantity,
-          });
-        }
-
+            unit_amount: Math.round(product.price * 100),
+          },
+          quantity: product.purchaseQuantity,
+        }));
+    
         const session = await stripe.checkout.sessions.create({
           payment_method_types: ['card'],
           line_items,
@@ -119,13 +114,13 @@ const resolvers = {
           success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
           cancel_url: `${url}/`,
         });
-
+    
         return { session: session.id };
       } catch (error) {
         console.error("Error creating checkout session:", error);
         throw error;
       }
-    },
+    },       
   },
   Mutation: {
     addUser: async (parent, args) => {
